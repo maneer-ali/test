3e11dea39e53b4488b898808ce05d085b1266466
Maneer Ali <98322380+maneer-ali@users.noreply.github.com>
2025-03-20 22:02:19 +0530

    Create main.py

---
+++ gcp-cicd-demo/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Cloud Build and Artifact Registry!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
