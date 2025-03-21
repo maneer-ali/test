167e338c3079812b70b3a5b367bd28ede9ad96b2
Maneer Ali <98322380+maneer-ali@users.noreply.github.com>
2025-03-21 17:22:33 +0530

    Update main.py

--- gcp-cicd-demo/main.py
+++ gcp-cicd-demo/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Cloud Build and Artifact Registry!"
+    return "Hello, new Cloud Build and Artifact Registry!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
